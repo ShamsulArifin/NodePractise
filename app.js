852293aceff73f1513d53255012a7fa6d11d74b0
Shamsul Arifin <arifinbd6@gmail.com>
2023-04-11 01:58:56 +0600

    exported module without declaring 'exports'

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 //CommonJs, every file is module (by default)
 //Modules - encapsulated Code (only share minimum)
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-
-sayHi('susan');
-sayHi(names.john);
-sayHi(names.peter);
+const names = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor');
+require('./7-mid-grenade')
