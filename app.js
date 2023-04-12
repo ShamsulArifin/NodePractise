ef284a6e97cbf967b93b97bf1f9bb00270e86a8c
Shamsul Arifin <arifinbd6@gmail.com>
2023-04-13 00:12:53 +0600

    aded syn and async file system modules

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-//CommonJs, every file is module (by default)
-//Modules - encapsulated Code (only share minimum)
-const names = require('./4-names');
-const sayHi = require('./5-utils');
-const data = require('./6-alternative-flavor');
-require('./7-mid-grenade')
+const { readFile, writeFile } = require('fs');
+
+//you have to setup callback to get the result from 'readFile' and 'writefile'
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  //provide encoding ('utf8' or something else) or else you will get buffer instead of text
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile('./content/second.txt', 'utf8', (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      './content/result-async.txt',
+      `Here is the result: ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
