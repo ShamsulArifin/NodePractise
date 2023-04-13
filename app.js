c267109834e65fbd0da36036830fb828ab29187a
Shamsul Arifin <arifinbd6@gmail.com>
2023-04-14 02:05:15 +0600

    tried lodash

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
-const { readFile, writeFile } = require('fs');
+const _ = require('lodash');
 
-//you have to setup callback to get the result from 'readFile' and 'writefile'
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  //provide encoding ('utf8' or something else) or else you will get buffer instead of text
-  if (err) {
-    console.log(err);
-    return;
-  }
-  const first = result;
-  readFile('./content/second.txt', 'utf8', (err, result) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const second = result;
-    writeFile(
-      './content/result-async.txt',
-      `Here is the result: ${first}, ${second}`,
-      (err, result) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log(result);
-      }
-    );
-  });
-});
+const items = [1, [2, [3, [4]]]];
+
+const newItems = _.flattenDeep(items);
+console.log(newItems);
